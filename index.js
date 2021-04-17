459ae629b36be66f71f010e5f132a759f3fbdf73
Sruthi Suresh <sruthisuresh321@gmail.com>
2021-04-17 22:44:16 +0530

    Add multiple export statements

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const add = (num1, num2) => num1 + num2
-const subtract = (num1, num2) => num1 - num2
-const multiply = (num1, num2) => num1 * num2
-const divide = (num1, num2) => num1 / num2;
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
 
-export { add, subtract, multiply, divide };
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+exports.divide = divide;
