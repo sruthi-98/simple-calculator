8bd8a90b8e16dd42ca5f85937a99c4fb8b67232c
Sruthi Suresh <sruthisuresh321@gmail.com>
2021-04-17 22:18:03 +0530

    Modify export statement

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const subtract = (num1, num2) => num1 - num2
 const multiply = (num1, num2) => num1 * num2
 const divide = (num1, num2) => num1 / num2;
 
-export const calculator = { add, subtract, multiply, divide };
+export { add, subtract, multiply, divide };
